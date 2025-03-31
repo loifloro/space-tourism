b0489e00cbec2f7f638a8b8085cdce4ec5d4f0f3
John Lois Floro <jlois.floro@gmail.com>
2025-03-31 03:48:29 +0000

    Add base property on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/space-tourism",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
